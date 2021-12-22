0f39fbcbcda4d658ea37bf5804a94296949c4641
crypticnfk <89737792+crypticnfk@users.noreply.github.com>
2021-12-22 22:26:17 +0530

    Updated contract to implement QF

--- contracts/AnbeShivamMain.sol
+++ contracts/AnbeShivamMain.sol
@@ -1,35 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+import "./AnbeShivamNFT.sol";
+import "./AnbeShivamInvestorToken.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
-interface IAnbeShivamNFT {
-    function safeMint(address _to, string memory _tokenURI) external;
-}
-
-interface IAnbeShivamInvestorToken {
-    function mint(address to, uint256 amount) external;
-}
 
 contract AnbeShivamMain is Ownable {
     address GODSContract;
     address NFTContract;
+    uint totalWeight;
 
     struct Content {
         uint id;
         uint receivedFunds;
+        uint matchedFunds;
+        uint weight;
+        uint votes;
+        bool isListed;
         string name;
         string fileURL;
         address payable creator;
     }
 
-    Content[] contents;
-
-    mapping(uint => string) public projects;
+    Content[] public contents;
+    uint public fundingPool;
+    uint public matchingPool;
 
     event newContentAdded(uint, string, string);
-    event viewedContent(Content, address);
     event projectFunded(uint, uint, address);
+    event setMatchingPool(uint);
+
+    function sqrt(uint y) internal pure returns (uint z) {
+        if (y > 3) {
+            z = y;
+            uint x = y / 2 + 1;
+            while (x < z) {
+                z = x;
+                x = (y / x + x) / 2;
+            }
+        } else if (y != 0) {
+            z = 1;
+        }
+    }
+
+    function syncMatchedFunds() external onlyOwner {
+        for(uint i=0; i < contents.length; ++i) {
+            if(contents[i].isListed) {
+                contents[i].matchedFunds = contents[i].weight*matchingPool/totalWeight;
+            }
+        }
+    } 
+
+    function returnContentCount() external view returns (uint) {
+        return contents.length;
+    } 
 
     function setContractAddresses(
         address _GODSContract, 
@@ -42,17 +66,24 @@ contract AnbeShivamMain is Ownable {
         NFTContract = _NFTContract;
     }
 
-    function returnContentCount() external view returns (uint) {
-        return contents.length;
+    function increaseMatchingPool() external payable onlyOwner {
+        require(msg.value > 0);
+        matchingPool += msg.value;
+        emit setMatchingPool(matchingPool);
     }
 
-    function returnContent(uint _contentID) external {
-        emit viewedContent(contents[_contentID], msg.sender);
-    } 
-
-    function addContent(string memory _name, string memory _fileURL) external {
-        contents.push(Content(contents.length, 0, _name, _fileURL, payable(msg.sender)));
-        projects[contents.length - 1] = _name;
+    function addContent(string memory _name, string memory _fileURL) external payable {
+        require(msg.value >= 2 ether, "Insufficient amount");
+        contents.push(
+            Content(
+                contents.length, 
+                0, 0, 0, 0, 
+                true, 
+                _name, 
+                _fileURL, 
+                payable(msg.sender)
+            )
+        );
         emit newContentAdded(
             contents.length - 1,
             contents[contents.length - 1].name,
@@ -61,12 +92,27 @@ contract AnbeShivamMain is Ownable {
     }
 
     function investFunds(uint _contentID, string memory _metadata) external payable {
-        require(msg.value > 0 && msg.value <= 1000000 ether, "Invalid amount");
-        require(msg.sender != contents[_contentID].creator, "Creator cannot invest");
+        require(msg.value >= 1 ether);
+        require(msg.sender != contents[_contentID].creator);
+        contents[_contentID].votes += 1;
+        totalWeight -= contents[_contentID].weight;
         contents[_contentID].receivedFunds += msg.value;
-        contents[_contentID].creator.transfer(msg.value);
-        IAnbeShivamInvestorToken(GODSContract).mint(msg.sender, msg.value);
-        IAnbeShivamNFT(NFTContract).safeMint(msg.sender, _metadata);
+        contents[_contentID].weight = (sqrt(contents[_contentID].weight + contents[_contentID].receivedFunds) + sqrt(msg.value))**2 - contents[_contentID].receivedFunds;
+        fundingPool += msg.value;
+        totalWeight += contents[_contentID].weight;
+        AnbeShivamInvestorToken(GODSContract).mint(msg.sender, msg.value);
+        AnbeShivamNFT(NFTContract).safeMint(msg.sender, _metadata);
         emit projectFunded(_contentID, msg.value, msg.sender);
+    }
+
+    function withdrawFunds(uint _contentID) external {
+        require(contents[_contentID].isListed, "Unlisted project");
+        require(msg.sender == contents[_contentID].creator, "Not creator");
+        uint totalFunds = contents[_contentID].receivedFunds + contents[_contentID].matchedFunds + 2 ether;
+        contents[_contentID].receivedFunds = 0;
+        matchingPool -= contents[_contentID].matchedFunds;
+        contents[_contentID].matchedFunds = 0;
+        contents[_contentID].isListed = false;
+        contents[_contentID].creator.transfer(totalFunds);
     }
 }
